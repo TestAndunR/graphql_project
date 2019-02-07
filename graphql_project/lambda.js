01d2221464f5bd9f7a1a471550639b7ac533c6b9
TestAndunR <andunranmal@gmail.com>
2019-02-07 10:42:19 +0530

    fix

--- graphql_project/lambda.js
+++ graphql_project/lambda.js
@@ -1,10 +1,10 @@
-const udith = require('sample-lambda-layer');
+const udith = require('sample-layer-udith');
 
 
 exports.handler = function(event, context, callback) {
     udith.getWeatherByCity("galle")
         .then(data => console.log(data))
-        .catch(err => console.log(err))
+        .catch(err => console.log)
     
     callback(null, {"message": "Successfully executed"});
 }
